ab2df107d75a473b5ff6c9cfa039f4a1ce3160dd
YouSay <yousay.sd@gmail.com>
2022-03-02 03:26:30 -0300

    feat: create Hero

---
+++ components/organisms/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import { Button, Container } from 'react-bootstrap'
+import ColorType from '../../atoms/ColorType/ColorType'
+import styles from './Hero.module.scss'
+
+const Hero = ({ type }) => {
+  return (
+    <section className={styles.hero}>
+      <Container className={styles.cta}>
+        <Link href='/'>
+          <a>
+            <Button>Back Home</Button>
+          </a>
+        </Link>
+      </Container>
+
+      <ColorType type={type} className={styles.color} />
+    </section>
+  )
+}
+
+export default Hero
